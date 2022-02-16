3c8ad01d84e9ed25f30750fad3e21ef5fc95bf96
Alene Margaret Onion <alene.onion@dec.ny.gov>
2022-02-16 07:34:22 -0500

    updates

--- scraps.R
+++ scraps.R
@@ -5,3 +5,9 @@ newdata %>% filter(CHARACTERISTIC_NAME=="TEMPERATURE",
                    DATA_PROVIDER=="LCI") %>% 
   filter(substr(SAMPLE_DATE,1,4)=='2021') %>% 
   select(SAMPLE_DATE) %>% distinct()
+
+junk<-lci %>% select(param,RSLT_RESULT_SAMPLE_FRACTION) %>% distinct()
+write.csv(junk,file="junk.fractions.csv",row.names=FALSE)
+
+
+rmarkdown::render("QAPP_Plots.Rmd")
