3ebcc0401cb9bc1512c5cf20a9c65b10cf675864
Alene Margaret Onion <alene.onion@dec.ny.gov>
2022-02-17 10:52:27 -0500

    updates

--- scraps.R
+++ scraps.R
@@ -11,3 +11,8 @@ write.csv(junk,file="junk.fractions.csv",row.names=FALSE)
 
 
 rmarkdown::render("QAPP_Plots.Rmd")
+
+junk<-newdata %>% 
+  filter(DATA_PROVIDER=="LCI",
+         substr(SAMPLE_DATE,1,4)=="2021",INFORMATION_TYPE %in% c('OW','BS')) %>% 
+  select(LAKE_HISTORY_ID,LOCATION_HISTORY_ID,INFORMATION_TYPE) %>% distinct()
